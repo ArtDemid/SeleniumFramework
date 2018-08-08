01dc2cffaf7faa59732861010a1ba44e05e8f832
Art Demid <artdemid@gmail.com>
2018-08-08 21:13:38 +0100

    v0.1

---
+++ SileniumTestIntro_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SileniumTestIntro_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("luxoft")]
+[assembly: AssemblyProduct("SileniumTestIntro_2")]
+[assembly: AssemblyCopyright("Copyright © luxoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8724b2de-227b-4f62-af39-aadf93c5417c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
